21218b0df3a095a27cfba67253cb1cd853c46da7
Vadym Babik <vadym.babik@gmail.com>
2021-01-21 19:22:56 +0200

    001

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 "use strict";
 //
-console.log("Как интересно!");
+console.log("Hello webpack!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,54 @@
 const { dirname } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const path = require("path");
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: "/js/index.js",
+  entry: ["/js/index.js", "/scss/style.scss"],
+  //
   output: {
-    filename: "[name].[hash].js",
+    filename: "js/[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "",
+    publicPath: "./",
   },
+  //
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, "src/js/"),
+  //   //   // compress: true,
+  //   port: 9000,
+  // },
+  //
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./index.html",
     }),
     new CleanWebpackPlugin(),
+    new ExtractTextPlugin({
+      minimize: true,
+    }),
   ],
+  module: {
+    rules: [
+      {
+        //scss
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: { sourceMap: true },
+            },
+            {
+              loader: "sass-loader",
+              options: { sourceMap: true },
+            },
+          ],
+          fallback: "style-loader",
+        }),
+      },
+    ],
+  },
 };
